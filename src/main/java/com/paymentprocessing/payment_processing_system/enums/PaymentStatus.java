581c369874f8f078b9f2a957ba744df0642ad9cf
paridhi02 <paridhigaba@gmail.com>
2026-08-05 07:56:59 +0000

    Added enums package

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/enums/PaymentStatus.java
@@ -0,0 +1,14 @@
+package com.paymentprocessing.payment_processing_system.enums;
+
+public enum PaymentStatus {
+
+    PENDING,
+    CREATED,
+    VALIDATED,
+    PROCESSING,
+    COMPLETED,
+    FAILED,
+    REFUNDED,
+    CANCELLED
+
+}
