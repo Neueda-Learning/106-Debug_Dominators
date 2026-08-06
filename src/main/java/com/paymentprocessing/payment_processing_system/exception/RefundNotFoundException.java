c83fe77a4d96d62da2759e51f10059ab2e400238
paridhi02 <paridhigaba@gmail.com>
2026-08-06 10:56:05 +0000

    Added RefundNotFoundException

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/exception/RefundNotFoundException.java
@@ -0,0 +1,8 @@
+﻿package com.paymentprocessing.payment_processing_system.exception;
+
+public class RefundNotFoundException extends RuntimeException {
+
+    public RefundNotFoundException(String message) {
+        super(message);
+    }
+}
