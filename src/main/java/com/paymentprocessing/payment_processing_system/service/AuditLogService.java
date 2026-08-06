9ac6ac8e64689625f490ab9148e59616e3de5d71
paridhi02 <paridhigaba@gmail.com>
2026-08-06 10:49:36 +0000

    Created AuditLogService interface

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/service/AuditLogService.java
@@ -0,0 +1,31 @@
+﻿package com.paymentprocessing.payment_processing_system.service;
+
+import com.paymentprocessing.payment_processing_system.dto.AuditLogRequest;
+import com.paymentprocessing.payment_processing_system.dto.AuditLogResponse;
+
+import java.util.List;
+
+public interface AuditLogService {
+
+
+    AuditLogResponse createAuditLog(
+            AuditLogRequest request);
+
+
+    List<AuditLogResponse> getAllAuditLogs();
+
+
+    AuditLogResponse getAuditLogById(Long id);
+
+
+    List<AuditLogResponse> getAuditLogsByPaymentId(
+            Long paymentId);
+
+
+    List<AuditLogResponse> getAuditLogsByEntity(
+            String entityName);
+
+
+    void deleteAuditLog(Long id);
+
+}
