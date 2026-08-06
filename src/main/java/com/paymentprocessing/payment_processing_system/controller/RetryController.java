ee405c118c6fd66b189089246be595cab670051a
paridhi02 <paridhigaba@gmail.com>
2026-08-06 11:01:23 +0000

    Added Swagger annotations to RetryController

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/controller/RetryController.java
@@ -0,0 +1,47 @@
+﻿package com.paymentprocessing.payment_processing_system.controller;
+
+
+import com.paymentprocessing.payment_processing_system.dto.RetryResponse;
+import com.paymentprocessing.payment_processing_system.service.RetryService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/retry")
+@Tag(
+        name = "Payment Retry APIs",
+        description = "APIs for retrying failed payments"
+)
+public class RetryController {
+
+
+    private final RetryService retryService;
+
+
+    public RetryController(
+            RetryService retryService) {
+
+        this.retryService = retryService;
+    }
+
+
+
+    @PostMapping("/{paymentId}")
+    @Operation(
+            summary = "Retry failed payment",
+            description = "Retries a failed payment and updates retry count"
+    )
+    public ResponseEntity<RetryResponse> retryPayment(
+            @PathVariable Long paymentId) {
+
+
+        return ResponseEntity.ok(
+                retryService.retryPayment(paymentId)
+        );
+    }
+}
