59db0e10fc833fc3737253f2b46cb0fff9bace59
paridhi02 <paridhigaba@gmail.com>
2026-08-06 10:56:04 +0000

    Added ProcessingException

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/exception/ProcessingException.java
@@ -0,0 +1,8 @@
+﻿package com.paymentprocessing.payment_processing_system.exception;
+
+public class ProcessingException extends RuntimeException {
+
+    public ProcessingException(String message) {
+        super(message);
+    }
+}
