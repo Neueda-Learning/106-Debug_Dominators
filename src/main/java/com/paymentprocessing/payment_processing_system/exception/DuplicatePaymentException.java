ba20a69ebf46311f348350ecadf75c92baf5f6b4
paridhi02 <paridhigaba@gmail.com>
2026-08-06 10:56:00 +0000

    Added DuplicatePaymentException

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/exception/DuplicatePaymentException.java
@@ -0,0 +1,8 @@
+﻿package com.paymentprocessing.payment_processing_system.exception;
+
+public class DuplicatePaymentException extends RuntimeException {
+
+    public DuplicatePaymentException(String message) {
+        super(message);
+    }
+}
