8e0eac040585e84e12d46df6b348084dca57aae0
paridhi02 <paridhigaba@gmail.com>
2026-08-06 10:56:05 +0000

    Added RefundException

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/exception/RefundException.java
@@ -0,0 +1,8 @@
+﻿package com.paymentprocessing.payment_processing_system.exception;
+
+public class RefundException extends RuntimeException {
+
+    public RefundException(String message) {
+        super(message);
+    }
+}
