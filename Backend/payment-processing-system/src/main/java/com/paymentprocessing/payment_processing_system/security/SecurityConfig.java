6ce8856b1b1da3701e3b7c1af3e61e2a39f6399b
Gantavya Rohatgi <gantavyarohatgi.work@gmail.com>
2026-08-06 09:37:35 +0000

    Corrected the CORS error

--- Backend/payment-processing-system/src/main/java/com/paymentprocessing/payment_processing_system/security/SecurityConfig.java
+++ Backend/payment-processing-system/src/main/java/com/paymentprocessing/payment_processing_system/security/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.paymentprocessing.payment_processing_system.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,8 +22,15 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
+                .cors(Customizer.withDefaults())
+
 
                 .authorizeHttpRequests(auth -> auth
+
+
+                        // CORS preflight - Public Access
+                        .requestMatchers(HttpMethod.OPTIONS, "/**")
+                        .permitAll()
 
 
                         // Swagger - Public Access
