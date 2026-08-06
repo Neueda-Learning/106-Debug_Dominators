10c1fa447b29bd2fccf92355fef940bf2fa49527
paridhi02 <paridhigaba@gmail.com>
2026-08-06 10:56:02 +0000

    Added InvalidAccountException

---
+++ src/main/java/com/paymentprocessing/payment_processing_system/exception/InvalidAccountException.java
@@ -0,0 +1,8 @@
+﻿package com.paymentprocessing.payment_processing_system.exception;
+
+public class InvalidAccountException extends RuntimeException {
+
+    public InvalidAccountException(String message) {
+        super(message);
+    }
+}
