06b628f16d93736765774ba42a25b4c53cc944dc
Sandeep U <159121732+Sandeep2k5@users.noreply.github.com>
2026-08-07 09:55:54 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [react(), tsconfigPaths()],
 });
